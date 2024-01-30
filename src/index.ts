7152db9052d1387b8caa948a6c930ea59b022aad
umaidashi <umaidashi18@gmail.com>
2024-01-30 14:14:35 +0900

    feat: add favicon

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono'
-
+import { serveStatic } from 'hono/cloudflare-workers'
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/', c => c.text('Hello Cloudflare Workers!'))
+
+app.get('/static/*', serveStatic({ root: './' }))
+app.get('/favicon.ico', serveStatic({ path: './favicon.ico' }))
 
 export default app
